5b29d2f1801b9904ced37c032a8db51a7c9a5e1b
Jitendra Sharma <jitendra.sharma2020@outlook.com>
2020-09-10 11:26:40 +0530

    Update settings.py

--- django_auth/django_auth/settings.py
+++ django_auth/django_auth/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,8 +74,8 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER=env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER=env('EMAIL_HOST_USER', default='EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=env('EMAIL_HOST_PASSWORD', default='EMAIL_HOST_PASSWORD')
 
 LOGIN_URL='/api/auth/signin'
 LOGIN_REDIRECT_URL='/api/auth/signin'
