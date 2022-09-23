8d6d8614fa65f67d4139ac9baec1c59634e2a48c
Jitendra Sharma <jitendra.sharma2020@outlook.com>
2022-09-23 15:05:55 +0530

    Create 3 user_levels ('Super-Admin', 'Teacher','Student') and add each user in specified user_level group.

--- django_auth/api/serializers.py
+++ django_auth/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth import password_validation
 
 
@@ -10,13 +10,19 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class SignUpSerializer(serializers.ModelSerializer):
+    USER_LEVELS = (
+        ("Super-Admin", "Super-Admin"),
+        ("Teacher", "Teacher"),
+        ("Student", "Student"),
+    )
     username = serializers.CharField(required=True, help_text='Username required')
-    email = serializers.EmailField(required=True, help_text='Email required')
     password = serializers.CharField(required=True, help_text='Password required')
+    email = serializers.EmailField(required=True, help_text='Email required')
+    user_level = serializers.ChoiceField(choices=USER_LEVELS, default="Student", help_text='User_Level required')
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'user_level')
         extra_fields = {'password': {'write_only':True}}
 
     def validate_email(self, email):
@@ -29,16 +35,27 @@ class SignUpSerializer(serializers.ModelSerializer):
         user = User.objects.filter(username=username).first()
         if user:
             raise serializers.ValidationError('Use another username.')
-        return email
+        return username
 
     def validate_password(self, password):
         password_validation.validate_password(password)
         return password
 
+    def to_representation(self, obj):
+        ret = super(ModelSerializer, self).to_representation(obj)
+        ret.pop('user_level')
+
     def create(self, validated_data):
+        group_name = validated_data['user_level']
+        is_superuser = False
+        is_staff = False
+
         user = User.objects.create_user(
             username = validated_data['username'],
             email = validated_data['email'],
-            password = validated_data['password']
+            password = validated_data['password'],
         )
+
+        group, group_status = Group.objects.get_or_create(name=group_name)
+        group.user_set.add(user)
         return user
